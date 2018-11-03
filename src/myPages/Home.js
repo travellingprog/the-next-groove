25f969d1159fd9987a3b7a6f1c664fe9004ade0b
travellingprog <e.cardenasmendez@gmail.com>
2018-11-02 20:54:13 -0400

    14: Create script to build home page JSON

--- src/myPages/Home.js
+++ src/myPages/Home.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
-import logo from 'myAssets/images/logo.svg'
 import './Home.css'
 
 const Home = () => (
-  <div className='my-Home'>
-    <header className='my-Home-header'>
-      <img src={logo} className='my-Home-logo' alt='logo' />
-      <h1 className='my-Home-title'>Welcome to React</h1>
-    </header>
-    <p className='my-Home-intro'>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+  <div className='tng-Home'>
+    Home here
   </div>
 )
 
